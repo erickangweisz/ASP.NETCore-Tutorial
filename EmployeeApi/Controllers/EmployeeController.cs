927e8d0755b3196fd62ac7187fb9c6bb4bb34941
jkang <erickangweisz@gmail.com>
2019-09-04 21:39:20 +0100

    Create EmployeeController without methods and use DI in the constructor to create a default employee if the database is empty and modify some attributes in employee model

---
+++ EmployeeApi/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeApi.Service.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            _context = context;
+
+            if (_context.Employees.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.Employees.Add(new Employee {
+                    Firstname = "Jonatan",
+                    Lastname = "Arrocha Kang",
+                    Email = "erickangweisz@gmail.com",
+                    Cellphone = "653 136 181"
+                });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
--- EmployeeApi/Models/Employee.cs
+++ EmployeeApi/Models/Employee.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
-        public string Address { get; set; }
+        public string Email { get; set; }
         public string Cellphone { get; set; }
     }
 }
