8d0b51cd67312c7fc0d00d68370d362880528f2f
jkang <erickangweisz@gmail.com>
2019-09-05 20:25:04 +0100

    Remove imports unused and all code commented

--- EmployeeApi/Controllers/EmployeeController.cs
+++ EmployeeApi/Controllers/EmployeeController.cs
@@ -1,11 +1,7 @@
 ﻿using EmployeeApi.Data.Models;
 using EmployeeApi.Data.Providers.Interfaces;
-using EmployeeApi.Service.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TodoApi.Controllers
 {
@@ -26,87 +22,5 @@ namespace TodoApi.Controllers
         {
             return employeeProvider.Get();
         }
-        /*private readonly EmployeeContext _context;
-
-        public EmployeeController(EmployeeContext context)
-        {
-            _context = context;
-
-            if (_context.Employees.Count() == 0)
-            {
-                // Create a new TodoItem if collection is empty,
-                // which means you can't delete all TodoItems.
-                _context.Employees.Add(new Employee {
-                    Firstname = "Jonatan",
-                    Lastname = "Arrocha Kang",
-                    Email = "erickangweisz@gmail.com",
-                    Cellphone = "653 136 181"
-                });
-                _context.SaveChanges();
-            }
-        }
-
-        // GET: api/employee
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
-        {
-            return await _context.Employees.ToListAsync();
-        }
-
-        // GET: api/employee/1
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Employee>> GetEmployee(int id)
-        {
-            var employee = await _context.Employees.FindAsync(id);
-
-            if (employee == null)
-            {
-                return NotFound();
-            }
-
-            return employee;
-        }
-
-        // POST: api/employee
-        [HttpPost]
-        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
-        {
-            _context.Employees.Add(employee);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetEmployee), new { id = employee.Id }, employee);
-        }
-
-        // PUT: api/employee/1
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutEmployee(int id, Employee employee)
-        {
-            if (id != employee.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(employee).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        // DELETE: api/employee/1
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteEmployee(int id)
-        {
-            var employee = await _context.Employees.FindAsync(id);
-
-            if (employee == null)
-            {
-                return NotFound();
-            }
-
-            _context.Employees.Remove(employee);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }*/
     }
 }
