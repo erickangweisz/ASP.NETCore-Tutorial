04829eb271f64e50643af36a08b8da5ceb625e54
jkang <erickangweisz@gmail.com>
2019-09-05 23:10:59 +0100

    Add query to create database and table employee with a few registers

---
+++ EmployeeApi.Data/employee_db_query.sql
@@ -0,0 +1,19 @@
+﻿CREATE DATABASE EmployeeDB
+
+USE EmployeeDB
+
+CREATE TABLE Employee
+(
+	Id INT PRIMARY KEY,
+	Firstname VARCHAR(30),
+	Lastname VARCHAR(40),
+	Email VARCHAR(40),
+	Cellphone VARCHAR(20)
+)
+
+INSERT INTO Employee VALUES 
+	(1, 'Jonatan', 'Arrocha Kang', 'erickangweisz@gmail.com', '653 136 181')
+INSERT INTO Employee VALUES 
+	(2, 'John', 'Doe', 'johndoe@gmail.com', '666 777 888')
+INSERT INTO Employee VALUES 
+	(3, 'Jane', 'Johns', 'janejohns@gmail.com', '666 555 444')
