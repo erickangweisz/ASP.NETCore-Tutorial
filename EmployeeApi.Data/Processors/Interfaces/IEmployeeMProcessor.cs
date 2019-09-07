e782deca4a8c422124b722a257cafa9fa31ad28f
jkang <erickangweisz@gmail.com>
2019-09-07 20:14:15 +0100

    Create IEmployeeMProcessor interface

---
+++ EmployeeApi.Data/Processors/Interfaces/IEmployeeMProcessor.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeApi.Data.Models;
+
+namespace EmployeeApi.Data
+{
+    public interface IEmployeeMProcessor
+    {
+        void Create(Employee employee);
+
+        void Update(Employee employee);
+
+        void Delete(int employeeId);
+    }
+}
