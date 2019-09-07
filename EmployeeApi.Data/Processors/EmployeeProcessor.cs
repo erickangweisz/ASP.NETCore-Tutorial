2e46ec0b8dd5d3c48c8b400041b804e6a8d8c9dc
jkang <erickangweisz@gmail.com>
2019-09-07 21:41:14 +0100

    Develop query into method to create an employee

---
+++ EmployeeApi.Data/Processors/EmployeeProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+using EmployeeApi.Data.Models;
+
+namespace EmployeeApi.Data.Processors
+{
+    public class EmployeeProcessor : IEmployeeMProcessor
+    {
+        private readonly string connectionString;
+
+        public EmployeeProcessor(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void Create(Employee employee)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Execute("INSERT INTO Employee (firstname, lastname, email, cellphone) VALUES (@Firstname, @Lastname, @Email, @Cellphone)",
+                    new { employee.Firstname, employee.Lastname, employee.Email, employee.Cellphone });
+            }
+        }
+
+        public void Delete(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
